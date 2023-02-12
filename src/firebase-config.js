b46eee092433f70dbc675609094372c288001747
Likhith Kolayari <likhith@regentmarkets.com>
2023-02-12 19:56:42 +0400

    feat: :art: added icon and TODOs

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -23,9 +23,6 @@ export const signInWithGoogle = setIsLoading => {
     setIsLoading(true);
     signInWithPopup(auth, google_provider)
         .then(result => {
-            // eslint-disable-next-line
-            console.log(result);
-
             const name = result.user.displayName;
             const email = result.user.email;
             const profile_img = result.user.photoURL;
@@ -35,6 +32,7 @@ export const signInWithGoogle = setIsLoading => {
             localStorage.setItem('profile_img', profile_img);
         })
         .catch(error => {
+            // [TODO:] Remove the console and handle the error
             // eslint-disable-next-line
             console.log(error);
         })
